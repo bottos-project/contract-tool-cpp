19c6ef781fe592b9e46a68ca65224a1f408d3dce
zhangwei <zhangwei@bottos.org>
2019-01-10 22:25:14 +0800

    rm data from db not support currently

--- lib/db.h
+++ lib/db.h
@@ -3,10 +3,8 @@
 
 extern "C" {
 
-uint32_t setBinValue(char* object, uint32_t objLen, char* key, uint32_t keyLen, char *value, uint32_t valLen);
+    uint32_t setBinValue(char* object, uint32_t objLen, char* key, uint32_t keyLen, char *value, uint32_t valLen);
 
-uint32_t getBinValue(char *contract, uint32_t contractLen, char* object, uint32_t objLen, char* key, uint32_t keyLen, char *valueBuf, uint32_t valueBufLen);
-
-uint32_t removeBinValue(char* object, uint32_t objLen, char* key, uint32_t keyLen);
+    uint32_t getBinValue(char *contract, uint32_t contractLen, char* object, uint32_t objLen, char* key, uint32_t keyLen, char *valueBuf, uint32_t valueBufLen);
 }
 
